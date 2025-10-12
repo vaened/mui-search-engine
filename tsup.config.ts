bdb3cba94d7a77d31c4a88284c657563189f0e10
Enea Dhack <enea@vaened.dev>
2025-10-12 14:24:29 -0500

    initial commit

---
+++ tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "tsup";
+import pkg from "./package.json";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  format: ["esm", "cjs"],
+  dts: true,
+  sourcemap: true,
+  clean: true,
+  treeshake: true,
+  external: [...Object.keys(pkg.peerDependencies || {}), ...Object.keys(pkg.dependencies || {})],
+});
