2a5fde3553bb439cf9fa49149ad2c754e3577dc5
Enea Dhack <enea@vaened.dev>
2025-10-20 21:44:35 -0500

    feat: add `useSearchEngineField` hook for field management
    Introduce `useSearchEngineField` hook to manage search engine fields. It registers fields in the store, provides a method to set values, and allows access to field metadata.

---
+++ src/hooks/useSearchEngineField.ts
@@ -0,0 +1,48 @@
+import { useSearchEngine } from "@/context";
+import type { FilterValue, RegisteredField, SerializedValue } from "@/types";
+import { useEffect, useMemo } from "react";
+
+export type UseSearchEngineFieldProps<V extends FilterValue, S extends SerializedValue> = Omit<RegisteredField<V, S>, "value"> & {
+  defaultValue: V;
+};
+
+export interface UseSearchEngineFieldResult<V extends FilterValue, S extends SerializedValue> {
+  value?: V;
+  field?: RegisteredField<V, S>;
+  set: (value: V) => void;
+}
+
+export function useSearchEngineField<V extends FilterValue, S extends SerializedValue>({
+  name,
+  humanize,
+  defaultValue,
+  serialize,
+  unserialize,
+}: UseSearchEngineFieldProps<V, S>): UseSearchEngineFieldResult<V, S> {
+  const { store, fields } = useSearchEngine();
+
+  const field = useMemo(() => (store.exists(name) ? (fields[name] as unknown as RegisteredField<V, S>) : undefined), [fields, name]);
+  const value = useMemo(() => field?.value, [field?.value]);
+
+  useEffect(() => {
+    store.register({
+      name,
+      value: defaultValue,
+      humanize,
+      serialize,
+      unserialize,
+    });
+
+    return () => store.unregister(name);
+  }, []);
+
+  function set(value: V) {
+    store.set(name, value);
+  }
+
+  return {
+    set,
+    field,
+    value,
+  };
+}
