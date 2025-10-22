fdbe0397031fd5104f36cd7ead4674d73196b0c1
Enea Dhack <enea@vaened.dev>
2025-10-22 18:26:16 -0500

    feat: add OptionSelect component for enhanced filtering
    Introduce OptionSelect component utilizing useSearchEngineField hook. Supports customizable value handling with optional serialization and humanization.

---
+++ src/components/OptionSelect.tsx
@@ -0,0 +1,52 @@
+/**
+ * @author enea dhack <contact@vaened.dev>
+ * @link https://vaened.dev DevFolio
+ */
+
+import { useSearchEngineField } from "@/hooks/useSearchEngineField";
+import type { FilterName, FilterValue, RegisteredField, SerializedValue } from "@/types";
+import { Select, type SelectProps } from "@mui/material";
+import type { SelectChangeEvent } from "node_modules/@mui/material";
+
+export type OptionSelectProps<V extends FilterValue, S extends SerializedValue> = Omit<SelectProps<V>, "value" | "name"> & {
+  name: FilterName;
+  defaultValue: V;
+  humanize?: RegisteredField<V, S>["humanize"];
+  serialize?: RegisteredField<V, S>["serialize"];
+  unserialize?: RegisteredField<V, S>["unserialize"];
+};
+
+export function OptionSelect<V extends FilterValue, S extends SerializedValue>({
+  name,
+  defaultValue,
+  children,
+  multiple,
+  humanize,
+  serialize,
+  unserialize,
+  ...props
+}: OptionSelectProps<V, S>) {
+  const { value, set } = useSearchEngineField({
+    name,
+    defaultValue,
+    humanize,
+    serialize,
+    unserialize,
+  });
+
+  function onOptionChange(event: SelectChangeEvent<V>) {
+    set(event.target.value as V);
+  }
+
+  return (
+    <Select
+      {...props}
+      onChange={onOptionChange}
+      multiple={multiple}
+      value={(value ? value : multiple ? [] : "") as V}
+      children={children}
+    />
+  );
+}
+
+export default OptionSelect;
