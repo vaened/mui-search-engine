23614090f69dfcab41c8fbfffbff14b351b1fa5a
Enea Dhack <enea@vaened.dev>
2025-10-31 19:08:02 -0500

    feat: integrate SearchEngineConfigProvider in main render function 🌐
    Replace React.StrictMode with SearchEngineConfigProvider in render. This sets the locale to "es" for the application.

--- playground/src/main.tsx
+++ playground/src/main.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import { SearchEngineConfigProvider } from "@/config";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+  <SearchEngineConfigProvider locale="es">
     <App />
-  </React.StrictMode>
+  </SearchEngineConfigProvider>
 );
