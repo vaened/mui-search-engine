e4fbee072059d4a3b08b23c1c37b604c34d3c2b7
Enea Dhack <enea@vaened.dev>
2025-10-31 19:07:52 -0500

    feat: add FilterFieldController component for field management
    Introduced a new component to manage filter fields. Utilizes the useFilterField hook to handle state and actions, passing values to a control function.

---
+++ src/components/FilterFieldController.tsx
@@ -0,0 +1,32 @@
+/**
+ * @author enea dhack <contact@vaened.dev>
+ * @link https://vaened.dev DevFolio
+ */
+
+import type { FieldStore } from "@/context/FieldStore";
+import { useFilterField } from "@/hooks/useFilterField";
+import type { Field, FilterValue, InferHumanizeReturn, InferSerializeReturn } from "@/types";
+import { type ReactElement } from "react";
+
+type Control<V extends FilterValue> = ({ value, set }: { value: V | null; set: (value: V) => void }) => ReactElement;
+
+export type FilterFieldControllerProps<V extends FilterValue, P extends InferSerializeReturn<V>, H extends InferHumanizeReturn<V>> = Omit<
+  Field<V, P, H>,
+  "value"
+> & {
+  store: FieldStore;
+  defaultValue?: V;
+  control: Control<V>;
+};
+
+export function FilterFieldController<V extends FilterValue, P extends InferSerializeReturn<V>, H extends InferHumanizeReturn<V>>({
+  store,
+  control,
+  ...restOfProps
+}: FilterFieldControllerProps<V, P, H>) {
+  const { value, set } = useFilterField(store, restOfProps);
+
+  return control({ value, set });
+}
+
+export default FilterFieldController;
