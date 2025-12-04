3bc92f8923265ce8f53289d37ab316bd162ec346
Enea Dhack <enea@vaened.dev>
2025-12-04 17:02:54 -0500

    fix: correct return type of unserialize method in AsynchronousSerializer
    The unserialize method's return type was adjusted to Promise<NoInfer<TValue> | undefined>. This ensures the method can return a promise of either a TValue or undefined.

--- packages/react/src/field.d.ts
+++ packages/react/src/field.d.ts
@@ -63,7 +63,7 @@ export type Humanizer<TValue, TResponse = HumanizeReturnType<TValue>> = (value: 
 
 export type AsynchronousSerializer<TValue> = {
   serialize(value: TValue): SerializeReturnType<TValue>;
-  unserialize(value: SerializeReturnType<TValue>): Promise<NoInfer<TValue | undefined>>;
+  unserialize(value: SerializeReturnType<TValue>): Promise<NoInfer<TValue> | undefined>;
 };
 
 export type SynchronousSerializer<TValue> = {
